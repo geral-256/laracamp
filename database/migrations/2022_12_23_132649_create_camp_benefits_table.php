99635f3b56242a73a5914112ddfc88e28caa8440
Geral Aditya <geral141100@gmail.com>
2022-12-23 20:42:05 +0700

    add migration,model,relations camp_benefit

---
+++ database/migrations/2022_12_23_132649_create_camp_benefits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('camp_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+
+            // Make a Foreign key (nama field),(tujuan),(nama table tujuan)
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+            //2nd method dengan syarat (nama table_namafield)
+            $table->foreignId('camps_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+};
